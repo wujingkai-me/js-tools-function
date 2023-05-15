c4107061388d51b2fc5e67b26379fb8fbc1c4157
wujingkaipro <1794533304@qq.com>
2023-05-15 19:09:58 +0800

    更新BUG

--- SelectDOM.js
+++ SelectDOM.js
@@ -4,9 +4,9 @@
  */
 function $(cssSelector) {
   if (/(1){1}[\.#]+[a-zA-z0-9].*?/.test(cssSelector)) {
-    return document.querySelectorAll(cssSelector.slice(1, cssSelector.length));
-  } else if (/[\.#]+[a-zA-z0-9].*?/.test(cssSelector)) {
     return document.querySelector(cssSelector.slice(1, cssSelector.length));
+  } else if (/[\.#]+[a-zA-z0-9].*?/.test(cssSelector)) {
+    return document.querySelectorAll(cssSelector);
   } else {
     throw TypeError("CSS 选择器有问题");
   }
