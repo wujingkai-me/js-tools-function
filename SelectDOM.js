a4081ad9072cc67107eb8655425bfad61c977aff
wujingkaipro <1794533304@qq.com>
2023-05-15 19:12:40 +0800

    更新BUG

--- SelectDOM.js
+++ SelectDOM.js
@@ -5,7 +5,7 @@
 function $(cssSelector) {
   if (/(1){1}[\.#]+[a-zA-z0-9].*?/.test(cssSelector)) {
     return document.querySelector(cssSelector.slice(1, cssSelector.length));
-  } else if (/[\.#]+[a-zA-z0-9].*?/.test(cssSelector)) {
+  } else if (/[\.#]?[a-zA-z0-9].*?/.test(cssSelector)) {
     return document.querySelectorAll(cssSelector);
   } else {
     throw TypeError("CSS 选择器有问题");
